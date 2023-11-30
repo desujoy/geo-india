68e995a82c2c32d0cf43c43ef22bfce82ee0f7d7
Sujoy De <99689289+desujoy@users.noreply.github.com>
2023-11-30 12:18:55 +0530

    init

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,11 @@
+import classes from './Header.module.css';
+
+const Header = () => {
+  return (
+    <header className={classes.heading}>
+      <h1>Geo India</h1>
+    </header>
+  );
+};
+
+export default Header;
